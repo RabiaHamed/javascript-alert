daa5137927d82b04f3b19494059e99f255884a1e
Rabia Hameed <nrsuriya093@gmail.com>
2023-10-01 01:20:19 -0700

    first commit

---
+++ alerts.js
@@ -0,0 +1,12 @@
+alert ( "Error! Pleace enter a valid password." );
+
+
+alert ( "Welcome to JS Land...\nHappy Coding!" );
+
+alert("Welcome to JS land...");
+
+
+alert("Happy Coding!\nPrevent this page from creating additional dialogs.");
+
+alert("Hello...I can run JS through my web brower's console .");
+
